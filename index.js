22c7279408c762387b1558b7576e7d05b84fcc9e
carloshcarniatto <carlohenrique@pcon.as>
2023-08-05 18:34:27 +0200

    created a generator for matriculas DGT

---
+++ index.js
@@ -0,0 +1,103 @@
+$('#btnGenerarMatricula').click(() => {
+    const randomNumericPart = generateRandomMatricula();
+    const randomLetterPart = generateRandomLetters(3);
+    const randomMatricula = `${randomNumericPart} ${randomLetterPart}`;
+    setGeneratedMatricula(randomMatricula);
+    addMatriculaToTable(randomMatricula);
+});
+
+$('#btnGenereteNextMatricula').click(() => {
+    const inputValue = getInputValue().toUpperCase();
+    const nextMatricula = generateNextMatricula(inputValue);
+    addMatriculaToDiv(addSpaceAtPosition(nextMatricula, 4));
+});
+
+function addSpaceAtPosition(str, position) {
+    if (position > 0 && position <= str.length) {
+        const firstPart = str.slice(0, position);
+        const secondPart = str.slice(position);
+        return `${firstPart} ${secondPart}`;
+    }
+    return str;
+}
+
+function addMatriculaToDiv(nextMatricula) {
+    $('#inputNextMatricula').append(`<div class='row-background'>${nextMatricula}</div>`);
+}
+
+function addMatriculaToTable(matricula) {
+    const newRow = `<tr><td><div class='row-background'>${matricula}</div></td></tr>`;
+    $('#generetedMatriculas tbody').append(newRow);
+}
+
+function generateRandomMatricula() {
+    const randomNumber = getRandomInt(0, 1000);
+    return formatNumber(randomNumber, 4);
+}
+
+function formatNumber(number, length) {
+    return number.toString().padStart(length, '0');
+}
+
+function generateRandomLetters(length) {
+    const alphabet = 'BCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let randomLetters = '';
+
+    for (let i = 0; i < length; i++) {
+        const randomIndex = getRandomInt(0, alphabet.length);
+        randomLetters += alphabet.charAt(randomIndex);
+    }
+
+    return randomLetters;
+}
+
+function generateNextLetter(value) {
+    const alphabet = 'BCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let nextDigitIndex = alphabet.indexOf(value);
+    let nextDigit = (nextDigitIndex === 24) ? 0 : nextDigitIndex + 1;
+    return alphabet.charAt(nextDigit);
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function generateNextDigit(digit) {
+    const alphabet = 'BCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const nextIndex = (alphabet.indexOf(digit) + 1) % alphabet.length;
+    const nextDigit = alphabet.charAt(nextIndex);
+    return nextDigit;
+}
+
+function generateNextThreeDigits(digit) {
+    let nextDigit = generateNextDigit(digit[2]);
+
+    if (nextDigit === 'B') {
+        let nextMiddleDigit = generateNextDigit(digit[1]);
+        if (nextMiddleDigit === 'B') {
+            let nextFirstDigit = generateNextDigit(digit[0]);
+            return `${nextFirstDigit}BB`;
+        }
+        return `${digit[0]}${nextMiddleDigit}B`;
+    }
+    return `${digit[0]}${digit[1]}${nextDigit}`;
+}
+
+function generateNextMatricula(inputValue) {
+    let numericDigits = parseInt(inputValue.substring(0, 4));
+    let lettersDigits = inputValue.substring(4, 7);
+    if (numericDigits === 9999) {
+        lettersDigits = generateNextThreeDigits(lettersDigits);
+    } else {
+        numericDigits += 1;
+    }
+    return `${formatNumber(numericDigits, 4)}${lettersDigits}`;
+}
+
+function setGeneratedMatricula(matricula) {
+    $('#inputGeneratedMatricula').val(matricula);
+}
+
+function getInputValue() {
+    return $('#inputMatricula').val();
+}
